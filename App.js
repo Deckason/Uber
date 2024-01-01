5871f94981c7f363356b5ade539e199ac088b0e7
Deckason <chibuikemchidera@gmail.com>
2024-01-01 20:04:55 +0100

    Development enviroment set for my Uber clone app now time to work on the app

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<>
+      <StatusBar 
+        style='light'
+        barStyle="light-content"
+        translucent={false}
+      />
+      <SafeAreaView style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </SafeAreaView>
+  </>);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
