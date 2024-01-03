a89a60cb276721609494f5ca09a75c7bfb8c36cf
Deckason <chibuikemchidera@gmail.com>
2024-01-03 10:52:11 +0100

    Implimented the GoogleplacesAutoComplete in the navigate card so when a user chooses destination, it would take the user to the rideOptions page with the map displaying the Origin and destination the user selected

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useRef, useEffect}from 'react'
 import MapView, {Marker} from 'react-native-maps';
 import { navStore } from '../store/navStore';
+import MapViewDirections from "react-native-maps-directions"
 
 const Map = () => {
-    const {origin} = navStore()
-    
+    const {origin, destination} = navStore()
+    const mapRef = useRef(null)
+
+    useEffect(()=>{
+        if(!origin || !destination) return
+
+        // Zoom and fit to screen
+        mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+            edgePadding: {top: 50, right: 50, bottom: 50, left: 50}
+        })
+    }, [origin, destination])
+
   return (
         <View style={styles.container}>
              <MapView
+                ref={mapRef}
                 style={styles.map}
                 mapType="mutedStandard"
                 initialRegion={{
@@ -18,13 +30,30 @@ const Map = () => {
                     longitudeDelta: 0.005,
                 }}
             >
+                {origin && destination && <MapViewDirections 
+                    origin={origin.description}
+                    destination={destination.description}
+                    apikey={process.env.GOOGLE_MAP_API_KEY}
+                    strokeWidth={3}
+                    strokeColor='black'
+                    
+                />}
                 {origin && <Marker
                     coordinate={{
                         latitude: origin.location.lat,
                         longitude: origin.location.lng,
                     }}
-                    identifier='Your Location'
+                    identifier='origin'
                     title='Origin'
+                    description={origin.description}
+                />}
+                {destination && <Marker
+                    coordinate={{
+                        latitude: destination.location.lat,
+                        longitude: destination.location.lng,
+                    }}
+                    identifier='destination'
+                    title='Destination'
                     description={origin.description}
                 />}
             </MapView>
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { navStore } from '../store/navStore';
+import { useNavigation } from '@react-navigation/native';
 
 const NavigateCard = () => {
+    const {updateDestination} = navStore()
+    const navigation = useNavigation()
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={styles.navCardContainer}>
+        <Text style={styles.welcomeText}>Welcome, Deckason</Text>
+        <View style={styles.cardsgoogleView}>
+            <GooglePlacesAutocomplete
+                styles={GooglePlacesStyles}
+                onPress={(data, details=null)=>{
+                    updateDestination({
+                        description: data.description, 
+                        location: details.geometry.location,
+                    })
+                    navigation.navigate("RideOptionsCard")
+                }}
+                placeholder='Where to?'
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+                fetchDetails={true}
+                returnKeyType={'search'}
+                minLength={2}
+                enablePoweredByContainer={false}
+                query={{
+                    // available options: https://developers.google.com/places/web-service/autocomplete
+                    key: process.env.GOOGLE_MAP_API_KEY,
+                    language: 'en', // language of the results
+                    // types: '(cities)' // default: 'geocode'
+                }}
+            />
+        </View>
+    </SafeAreaView>
   )
 }
 
 export default NavigateCard
 
-const styles = StyleSheet.create({})
+const GooglePlacesStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        fontSize: 18,
+        borderRadius: 0,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0
+    }
+})
+
+const styles = StyleSheet.create({
+    navCardContainer: {
+        height: "100%",
+        backgroundColor: "#fff",
+    },
+    welcomeText: {
+        textAlign: "center",
+        fontSize: 20,
+        paddingVertical: 5,
+    },
+    cardsgoogleView: {
+        borderTopColor: "lightgray",
+
+    },
+})
